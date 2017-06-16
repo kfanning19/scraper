da6b13c1bba15249215f36706faf33465e38dca3
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-06-16 06:10:21 -0500

    working navbar

---
+++ controllers/scraper.js
@@ -0,0 +1,4 @@
+// Requiring our Note and Article models
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
+
---
+++ server.js
@@ -0,0 +1,50 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/scraper");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraper.js");
+
+app.use("/", routes);
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
