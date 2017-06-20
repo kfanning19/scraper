6423b53069e1196d1ad39bd47d7015b423bc1caa
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-06-20 17:11:06 -0500

    update handlebars and js

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -12,7 +12,7 @@ var mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/mongoScraper");
+mongoose.connect(" mongodb://heroku_vbq5x4mc:5ttr0neh288ibk59j1e8dajvmr@ds149221.mlab.com:49221/heroku_vbq5x4mc");
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -32,59 +32,10 @@ module.exports = function(app) {
         res.redirect("/articles")
 
     });
-    app.get("/saved", function(req, res) {
-        Article.find({saved: true}, {multi: true}).populate().exec(function(err, docs) {
-            if (err) {
-                console.log(err)
-            } else {
-                res.render("saved", { articles: docs })
-            }
-        })
-
-    });
-    // A GET request to scrape the echojs website
-    app.get("/scrape", function(req, res) {
-
-                request("http://www.npr.org/sections/news/", function(error, response, html) {
-                    // Then, we load that into cheerio and save it to $ for a shorthand selector
-                    var $ = cheerio.load(html);
-                    // Now, we grab every h2 within an article tag, and do the following:
-                    $("h2.title").each(function(i, element) {
-
-                        // Save an empty result object
-                        var result = {};
-
-                        // Add the text and href of every link, and save them as properties of the result object
-                        result.title = $(this).children("a").text();
-                        result.link = $(this).children("a").attr("href");
-                        result.summary = $(this).sibling("p").text();
-
-                        // Using our Article model, create a new entry
-                        // This effectively passes the result object to the entry (and the title and link)
-                        var entry = new Article(result);
-
-                        // Now, save that entry to the db
-                        entry.save(function(err, doc) {
-                            // Log any errors
-                            if (err) {
-                                console.log(err);
-                            }
-                            // Or log the doc
-                            else {
-                                console.log(doc);
-                            }
-                        });
-
-                    });
-                });
-                // Tell the browser that we finished scraping the text
-                res.render("home", { articles: doc });
-    });
-
     // This will get the articles we scraped from the mongoDB
     app.get("/articles", function(req, res) {
         // Grab every doc in the Articles array
-        Article.find({}).sort(sort({ "created": -1 })).exec(function(error, doc) {
+        Article.find({}).sort({ "created": -1 }).exec(function(error, doc) {
             // Log any errors
             if (error) {
                 console.log(error);
@@ -95,6 +46,60 @@ module.exports = function(app) {
             }
         });
     });
+    app.get("/saved", function(req, res) {
+        // Grab every doc in the Articles array
+        Article.find({}).sort({ "created": -1 }).exec(function(error, doc) {
+            // Log any errors
+            if (error) {
+                console.log(error);
+            }
+            // Or send the doc to the browser as a json object
+            else {
+                res.render("saved", { articles: doc });
+            }
+        });
+    });
+    // A GET request to scrape the echojs website
+    app.get("/scrape", function(req, res) {
+
+        request("http://www.npr.org/sections/news/", function(error, response, html) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(html);
+            // Now, we grab every h2 within an article tag, and do the following:
+            $("h2.title").each(function(i, element) {
+
+                // Save an empty result object
+                var result = {};
+
+                // Add the text and href of every link, and save them as properties of the result object
+                result.title = $(this).children("a").text();
+                result.link = $(this).children("a").attr("href");
+                result.summary = $(this).siblings("p").text();
+
+                // Using our Article model, create a new entry
+                // This effectively passes the result object to the entry (and the title and link)
+                var entry = new Article(result);
+
+                // Now, save that entry to the db
+                entry.save(function(err, doc) {
+                    // Log any errors
+                    if (err) {
+                        console.log(err);
+                    }
+                    // Or log the doc
+                    else {
+                        console.log(doc);
+                    }
+                });
+
+            });
+            res.send("Scrape Complete")
+        });
+        // Tell the browser that we finished scraping the text
+
+    });
+
+
 
     // Grab an article by it's ObjectId
     app.get("/articles/:id", function(req, res) {
@@ -114,29 +119,33 @@ module.exports = function(app) {
                 }
             });
     });
-    app.post("save/articles/:id", function(req, res){
-        Article.findOneAndUpdate({ "_id": req.params.id }, {$set:{saved: true}}).exec(function(err, doc){
-            if(err){
-                console.log(err)
-            }else{
-                res.json(doc);
-            }
-        })
+    app.post("/save/article/:id", function(req, res) {
+        Article.findOneAndUpdate({ "_id": req.params.id }, { $set: { "saved": true } })
+            .exec(function(err, doc) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.send(doc);
+                }
+            })
     });
-        app.post("unsave/articles/:id", function(req, res){
-        Article.findOneAndUpdate({ "_id": req.params.id }, {$set:{saved: false}}).exec(function(err, doc){
-            if(err){
-                console.log(err)
-            }else{
-                res.json(doc);
-            }
-        })
+    app.post("/unsave/article/:id", function(req, res) {
+        Article.findOneAndUpdate({ "_id": req.params.id }, { $set: { "saved": false } })
+            .exec(function(err, doc) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.send(doc);
+                }
+            })
     });
 
     // Create a new note or replace an existing note
-    app.post("/articles/:id", function(req, res) {
+    app.post("/save/note/:id", function(req, res) {
         // Create a new note and pass the req.body to the entry
-        var newNote = new Note(req.body);
+        var newNote = new Note(
+            body: req.body.text,
+            article: req.params.id);
 
         // And save the new note the db
         newNote.save(function(error, doc) {
@@ -147,7 +156,7 @@ module.exports = function(app) {
             // Otherwise
             else {
                 // Use the article id to find and update it's note
-                Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+                Article.findOneAndUpdate({ "_id": req.params.id }, { "notes": doc._id })
                     // Execute the above query
                     .exec(function(err, doc) {
                         // Log any errors
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,42 @@
-$("#scrape-btn").on("click", function() {
-    $.ajax({
-        method: "GET",
-        url: "/scrape"
-    }).done(function(data) {
-        $("#scrapeModal").modal();
-    })
+$(".save-btn").on("click", function(){
+	var articleId = $(this).attr("data-id");
+	$.ajax({
+		method: "POST",
+		url: "/save/article/" + articleId
+	}).done(function(){
+		window.location.reload();
+	})
+});
+
+$("#scrape-btn").on("click", function(){
+	
+	$.ajax({
+		method: "GET",
+		url: "/scrape" 
+	}).done(function(){
+		window.location.reload();
+		$("#scrapeModal").modal();
+	})
+});
+
+$(".unsave-btn").on("click", function(){
+	var articleId = $(this).attr("data-id");
+	$.ajax({
+		method:"POST",
+		url: "/unsave/article/" + articleId
+	}).done(function(){
+		window.location.reload();
+	})
+	})
+});
+
+$(document).on("click", ".newNote-btn" function(event){
+	event.preventDefault();
+	var articleId= $(this).attr("data-id");
+	$.ajax({
+		method: "POST",
+		url: "/save/note/" + articleId
+	}).done(
+		window.location.reload();
+	)
 })
